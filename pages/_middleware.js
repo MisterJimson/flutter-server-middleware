cd24c1298f1612e7d38c2538d600ffde98e2f139
Jason Rai <me@jasonrai.ca>
2022-05-14 13:44:34 -0400

    feat: cleanup, use example app url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,14 +1,17 @@
 import { NextResponse } from "next/server";
 
+const flutterBaseUrl = "https://flutter-seo-example.vercel.app";
+
 export default async function middleware(req) {
   const url = req.nextUrl.clone();
+  const externalUrl = `${flutterBaseUrl}${url.pathname}`;
 
+  // Need better way to determine if the request is the inital page load
   if (url.toString().endsWith("/")) {
-    const externalUrl = `https://gallery.flutter.dev${url.pathname}`;
     const externalContent = await fetch(externalUrl);
-
     const externalContentString = await externalContent.text();
 
+    // Add SEO tags to the initial page load
     var result = externalContentString.replace(
       "<head>",
       '<head>\n<meta name="author" content="John Doe">'
@@ -18,6 +21,6 @@ export default async function middleware(req) {
       headers: externalContent.headers,
     });
   } else {
-    return NextResponse.rewrite(`https://gallery.flutter.dev${url.pathname}`);
+    return NextResponse.rewrite(externalUrl);
   }
 }
